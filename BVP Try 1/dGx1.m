9c1a0f0a486cdc21cbdb9c98157ea4e1a3cd373a
hydanggia2899 <62788726+hydanggia2899@users.noreply.github.com>
2020-07-09 17:05:45 +0700

    Fixed some bugs in dG/dx1

--- BVP Try 1/dGx1.m
+++ BVP Try 1/dGx1.m
@@ -7,31 +7,28 @@ x0_3 = output_IVP.x3(1,:);
 x1_3 = output_IVP.x3(end,:);
 
 %for the first 3 element of G (derivatives) over x(1) 
-dG_x1(1:3,1+6*(n-1):6*(n-1)+3) = eye(3,3);
+dG_x1(1:3,1+6*(n-1):6*(n-1)+3) = eye(3,3)
 
 %for the next 3*(n-1) elements for the boundary conditions 
 for i=1:n-1
-    for j=1:n-1 
-        dG_x1(3*i+1:3*i+3,6*(j-1)+1: 6*j-3) = -eye(3,3);
-    end
+        dG_x1(3*i+1:3*i+3,6*(i-1)+1: 6*i-3) = -eye(3,3)
 end
 
 %for the boundary on p's 
 
 for i = 1:n-1
-    for j=1:n-1 
-        dG_x1(3*(n-1)+3*i+1:3*(n-1)+3*i+3,4+6*(j-1): 6*j) = -eye(3,3);
-    end
+        dG_x1(3*(n-1)+3*i+1:3*(n-1)+3*i+3,4+6*(i-1): 6*i) = -eye(3,3)
+end
 
 %for the last func
 for i=1:n-1
-   for j=1:n-1
-       dG_x1(6*(n-1)+3+i:6*(n-1)+3+i,4+6*(j-1): 6*j) = [0 -cos(x0_3(i+1)) -sin(x0_3(i+1))];
-   end
+       dG_x1(6*(n-1)+3+i:6*(n-1)+3+i,4+6*(i-1): 6*i) = [0 -cos(x0_3(i+1)) -sin(x0_3(i+1))];
 end
 
 
-output = dG_x1;
+output = dG_x1
 
 end
+
+
 
